a7825dc391ad25c5766e2a3db77e60ca1e2e846a
Jon Kartago Lamida <me@lamida.net>
2014-07-11 10:59:07 +0700

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 learn-c-hard-way
 ================
+
+Playground code for c.learncodethehardway.org
---
+++ ex5/ex5.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/*comment*/
+int main(int argc, char *argv[])
+{
+    int distance = 100;
+
+    // another comment
+    printf("you are %d miles away.\n", distance);
+
+    return 0;
+}
+
